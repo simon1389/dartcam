41da6398f63855412c3cbf0b58b247d911f9f6a0
simon1389 <simon.hoefle@gmail.com>
2017-12-18 21:42:12 +0100

    opengl raus.. is kacke

--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,13 @@ import java.awt.*;
 
 public class Main {
     public static void main(String args[]) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
-        System.setProperty("sun.java2d.opengl", "true");
+        //System.setProperty("sun.java2d.opengl", "true");
         System.setProperty("sun.java2d.accthreshold","0");
         System.setProperty("apple.laf.useScreenMenuBar", "true");
-
-        System.out.println("max " + Runtime.getRuntime().maxMemory()/(1024*1024));
-        System.out.println("free " + Runtime.getRuntime().freeMemory()/(1024*1024));
-        System.out.println("total " + Runtime.getRuntime().totalMemory()/(1024*1024));
 
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         DartMainForm form = new DartMainForm();
         SwingUtilities.invokeLater(form);
-
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                SplashScreen splash = SplashScreen.getSplashScreen();
-//                if (splash == null) {
-//                    System.out
-//                            .println("SplashScreen kann nicht erzeugt werden.");
-//                    return;
-//                }
-//                try {
-//                    Thread.sleep(5000);
-//                } catch (InterruptedException e) {
-//                    System.err.println("Thread unterbrochen");
-//                }
-//                splash.close();
-//                form.setVisible(true);
-//            }
-//        }).start();
-
-//        boolean found = new NativeDiscovery().discover();
-//        System.out.println(found);
-//        System.out.println(LibVlc.INSTANCE.libvlc_get_version());
 
 
 //        Tesseract instance = Tesseract.getInstance();
